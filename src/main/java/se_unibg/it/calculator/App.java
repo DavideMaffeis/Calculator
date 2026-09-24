9a0283827ecae7e2996cf7dcabb5904988335048
DavideMaffeis <d.maffeis10@studenti.unibg.it>
2026-09-24 17:05:59 +0200

    aggiunta classe

---
+++ src/main/java/se_unibg/it/calculator/App.java
@@ -0,0 +1,12 @@
+package se_unibg.it.calculator;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        Mecca filippo = new Mecca();
+        System.out.println("Anni di Mecca: PAGURI");
+        filippo.getAnni();
+    }
+}
---
+++ src/main/java/se_unibg/it/calculator/Mecca.java
@@ -0,0 +1,13 @@
+package se_unibg.it.calculator;
+
+
+public class Mecca {
+	private int anni;
+	public Mecca() {
+		this.anni = 10;
+	}
+	
+	public int getAnni() {
+		return this.anni;
+	}
+}
